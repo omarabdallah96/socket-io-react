da017e03f2ab19508c677488377c2ff1eb7f5419
Omar Abdallah <71341894+omarabdallah96@users.noreply.github.com>
2021-08-02 21:53:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function App() {
 
 	return (
 		<>
-			<h1 style={{ textAlign: "center", color: '#fff' }}>Zoomish</h1>
+			<h1 style={{ textAlign: "center", color: '#fff' }}>Codi Tech</h1>
 		<div className="container">
 			<div className="video-container">
 				<div className="video">
